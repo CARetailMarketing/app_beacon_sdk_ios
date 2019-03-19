974eb9e1f528d02b5b39a9d0cb5f0b8a48b8fed1
近藤 寛志 <kondo@cloudpack.jp>
2019-03-19 17:53:05 +0900

    initial commit.

---
+++ BeaconStamp/BeaconStamp.h
@@ -0,0 +1,19 @@
+//
+//  BeaconStamp.h
+//  BeaconStamp
+//
+//  Created by 近藤 寛志 on 2019/03/19.
+//  Copyright © 2019 iret,Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BeaconStamp.
+FOUNDATION_EXPORT double BeaconStampVersionNumber;
+
+//! Project version string for BeaconStamp.
+FOUNDATION_EXPORT const unsigned char BeaconStampVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BeaconStamp/PublicHeader.h>
+
+
---
+++ README.md
@@ -0,0 +1,4 @@
+# Requirements
+- iOS 9.0+
+- Xcode 10.1+
+- Swift 4.2+
